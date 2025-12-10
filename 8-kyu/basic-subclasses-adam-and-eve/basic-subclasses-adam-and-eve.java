d8a9982cdd8987398af1e8b96275dcce9b6b6b5f
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2025-12-10 18:21:01 +0200

    Add solution - CodeHub

---
+++ 8-kyu/basic-subclasses-adam-and-eve/basic-subclasses-adam-and-eve.java
@@ -0,0 +1,18 @@
+public class God {
+  public static Human[] create(){
+        Human adam = new Man();
+        Human eve = new Woman();
+        return new Human[]{adam, eve};
+    }
+}
+class Human{
+​
+​
+}
+class Man extends Human{
+​
+}
+class Woman extends Human{
+​
+}
+​
