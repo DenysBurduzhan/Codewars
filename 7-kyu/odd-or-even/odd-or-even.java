f36418c2afeb47c5058f96fb20815dc00b49d370
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2025-12-23 18:14:32 +0200

    Update solution - CodeHub

--- 7-kyu/odd-or-even/odd-or-even.java
+++ 7-kyu/odd-or-even/odd-or-even.java
@@ -1,14 +1,7 @@
+import java.util.Arrays;
 public class Codewars {
-​
-    public static String oddOrEven (int[] array) {
-        int sum = 0;
-        for (int j : array) {
-            sum += j;
-        }
-        if(sum % 2 == 0){
-            return "even";
-        }
-​
-        return "odd";
+   public static String oddOrEven (int[] array) {
+        String str ;
+        return str = Arrays.stream(array).sum() % 2 == 0 ? "even" : "odd";
     }
-  }
+}
