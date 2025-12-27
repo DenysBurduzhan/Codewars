f6268c0ae0283b1c75d46bee050f109a9554ce0b
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2025-12-27 18:45:06 +0200

    Add solution - CodeHub

---
+++ 8-kyu/sum-of-positive/sum-of-positive.java
@@ -0,0 +1,13 @@
+public class Positive{
+​
+  public static int sum(int[] arr){
+        int sum = 0;
+        for (int j : arr) {
+            if (j > 0) {
+                sum += j;
+            }
+        }
+        return sum;
+    }
+​
+}
