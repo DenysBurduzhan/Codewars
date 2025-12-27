3e31004b7633d6e118f37721aa29b7bc4a552aa4
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2025-12-27 18:37:03 +0200

    Update solution - CodeHub

--- 8-kyu/string-repeat/string-repeat.java
+++ 8-kyu/string-repeat/string-repeat.java
@@ -1,12 +1,8 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 public class Solution {
     public static String repeatStr(final int repeat, final String string) {
-       
-        StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < repeat; i++) {
-        sb.append(string);
-    }
-    return sb.toString();
-​
+       return IntStream.range(0, repeat).mapToObj(i -> string).collect(Collectors.joining());
     }
 }
 ​
