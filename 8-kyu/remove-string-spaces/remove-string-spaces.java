5f6e238300ba6b7dcd0b1b7286872bc6cb081f69
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2025-12-29 22:28:44 +0200

    Update solution - CodeHub

--- 8-kyu/remove-string-spaces/remove-string-spaces.java
+++ 8-kyu/remove-string-spaces/remove-string-spaces.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
 public class Kata {
     public static String noSpace(final String x) {
-        return x.replace(" ", "");
+       return Arrays.stream(x.split("")) 
+                .filter(ch -> !ch.equals(" ")) 
+                .collect(Collectors.joining());
     }
 }
