734c1b6bedd65cea40d94d8ea0e4436df017795b
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-11 16:16:07 +0200

    Update solution - CodeHub

--- 8-kyu/find-the-smallest-integer-in-the-array/find-the-smallest-integer-in-the-array.java
+++ 8-kyu/find-the-smallest-integer-in-the-array/find-the-smallest-integer-in-the-array.java
@@ -1,12 +1,6 @@
+import java.util.Arrays;
 public class SmallestIntegerFinder {
     public static int findSmallestInt(int[] args) {
-         int smallest = Integer.MAX_VALUE;
-​
-        for(int num : args){
-            if(num < smallest){
-                smallest = num;
-            }
-        }
-        return smallest;
+        return Arrays.stream(args).min().getAsInt();
     }
 }
