d66b47501aaefdd901cb353afa333b7499fbac72
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-12 14:51:50 +0200

    Update solution - CodeHub

--- 8-kyu/fake-binary/fake-binary.java
+++ 8-kyu/fake-binary/fake-binary.java
@@ -1,14 +1,5 @@
 public class FakeBinary {
     public static String fakeBin(String numberString) {
-        StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < numberString.length(); i++){
-            int n = Integer.parseInt(String.valueOf(numberString.charAt(i)));
-            if(n < 5){
-                builder.append(0);
-            }else{
-                builder.append(1);
-            }
-        }
-        return builder.toString();
+         return numberString.replaceAll("[1-4]", "0").replaceAll("[5-9]", "1");
     }
 }
