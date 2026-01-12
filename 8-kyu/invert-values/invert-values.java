1b54e2b2c027f1bce7179ab25da4a8728baafe65
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-12 13:46:40 +0200

    Update solution - CodeHub

--- 8-kyu/invert-values/invert-values.java
+++ 8-kyu/invert-values/invert-values.java
@@ -1,9 +1,6 @@
+import java.util.Arrays;
 public class Kata {
   public static int[] invert(int[] array) {
-   int[] reversed = new int[array.length];
-        for(int i = 0; i < array.length; i++){
-            reversed[i] = array[i] * -1;
-        }
-        return reversed;
+    return Arrays.stream(array).map(i -> i * -1).toArray();
   }
 }
