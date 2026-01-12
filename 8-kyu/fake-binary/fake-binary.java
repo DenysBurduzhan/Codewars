add8434047ad8f2e22babc0d1df568b9e259ccf3
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-12 16:15:54 +0200

    Update solution - CodeHub

--- 8-kyu/fake-binary/fake-binary.java
+++ 8-kyu/fake-binary/fake-binary.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
 public class FakeBinary {
     public static String fakeBin(String numberString) {
-         return numberString.replaceAll("[1-4]", "0").replaceAll("[5-9]", "1");
+        return Arrays.stream(numberString.split("")).map(i -> i.replaceAll("[1-4]", "0").replaceAll("[5-9]", "1")).collect(Collectors.joining());
     }
 }
