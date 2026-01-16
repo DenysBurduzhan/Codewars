cfcaaf851856330dfcb5caee3ea90685b42b2ec9
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-16 20:05:35 +0200

    Update solution - CodeHub

--- 8-kyu/you-only-need-one-beginner/you-only-need-one-beginner.java
+++ 8-kyu/you-only-need-one-beginner/you-only-need-one-beginner.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class Solution {
 ​
     public static boolean check(Object[] a, Object x) {
-       return Arrays.asList(a).contains(x);
+        return Arrays.stream(a).anyMatch(i -> i.equals(x));
     }
 ​
 }
