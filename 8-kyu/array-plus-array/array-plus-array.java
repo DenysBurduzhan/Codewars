b5242c6bae3d0720c67c1fbebf6fd576f3153096
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-16 20:24:21 +0200

    Update solution - CodeHub

--- 8-kyu/array-plus-array/array-plus-array.java
+++ 8-kyu/array-plus-array/array-plus-array.java
@@ -1,9 +1,7 @@
-import java.util.Arrays;
 import java.util.stream.IntStream;
 public class Sum {
 ​
-  public static int arrayPlusArray(int[] arr1, int[] arr2) {
-    return IntStream.concat(Arrays.stream(arr1), Arrays.stream(arr2)).sum();
-  }
-​
+    public static int arrayPlusArray(int[] arr1, int[] arr2){
+        return IntStream.of(arr1).sum() + IntStream.of(arr2).sum();
+    }
 }
