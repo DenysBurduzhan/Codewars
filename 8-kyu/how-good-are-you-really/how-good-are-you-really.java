93916e3ef7d0ee01b5fee1a504e37c25c3e90267
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-17 17:00:55 +0200

    Update solution - CodeHub

--- 8-kyu/how-good-are-you-really/how-good-are-you-really.java
+++ 8-kyu/how-good-are-you-really/how-good-are-you-really.java
@@ -1,13 +1,6 @@
+import java.util.Arrays;
 public class Kata {
   public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
-        return yourPoints > Kata.sum(classPoints) / classPoints.length;
-    }
-    
-    public static int sum(int[] classPoints){
-        int sum = 0;
-        for(int i : classPoints){
-            sum += i;
-        }
-        return sum;
-    }
+    return Arrays.stream(classPoints).sum() / classPoints.length < yourPoints;
+  }
 }
