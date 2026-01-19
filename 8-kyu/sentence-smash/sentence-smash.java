1ff2a14c830c1a3ec33cf69f545526f2395ec09b
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-19 20:34:22 +0200

    Update solution - CodeHub

--- 8-kyu/sentence-smash/sentence-smash.java
+++ 8-kyu/sentence-smash/sentence-smash.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
 public class SmashWords {
 ​
   public static String smash(String... words) {
-    return String.join(" ", words);
+    return Arrays.stream(words).collect(Collectors.joining(" "));
   }
 }
