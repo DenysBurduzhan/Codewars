95e94a6fb014a3c300aad20ecffc24d47f9ddecf
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-19 20:26:58 +0200

    Update solution - CodeHub

--- 8-kyu/sentence-smash/sentence-smash.java
+++ 8-kyu/sentence-smash/sentence-smash.java
@@ -1,14 +1,6 @@
 public class SmashWords {
 ​
   public static String smash(String... words) {
-    StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < words.length; i++){
-            if(i < words.length -1){
-                builder.append(words[i]).append(" ");
-            }else{
-                builder.append(words[i]);
-            }
-        }
-        return builder.toString() ;
+    return String.join(" ", words);
   }
 }
