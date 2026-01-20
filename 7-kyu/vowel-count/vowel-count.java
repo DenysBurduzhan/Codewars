1aabfa9c79fb5ed9aa8210c9cdb86462d72c6e21
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-20 19:34:45 +0200

    Update solution - CodeHub

--- 7-kyu/vowel-count/vowel-count.java
+++ 7-kyu/vowel-count/vowel-count.java
@@ -1,14 +1,18 @@
- public class Vowels {
+import java.util.Arrays;
+import java.util.HashSet;
+public class Vowels {
 ​
-   public static int getCount(String str) {
+  public static int getCount(String str) {
+        HashSet<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
         int count = 0;
-        for(int i = 0; i< str.length(); i++){
+        for(int i =0; i < str.length(); i++){
             String s = String.valueOf(str.charAt(i));
-            if(s.matches(".*[aeiou]")){
-                count++;
+            for(char f : set){
+                if(s.contains(String.valueOf(f))){
+                    count++;
+                }
             }
         }
         return count;
     }
-​
 }
