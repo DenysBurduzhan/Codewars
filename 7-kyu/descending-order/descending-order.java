d949427e9a1213d93aa12a654e3a104636163808
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-21 20:14:56 +0200

    Update solution - CodeHub

--- 7-kyu/descending-order/descending-order.java
+++ 7-kyu/descending-order/descending-order.java
@@ -1,16 +1,8 @@
- public class DescendingOrder {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+public class DescendingOrder {
   public static int sortDesc(final int num) {
-    String[] str = String.valueOf(num).split("");
-        for (int i = 0; i < str.length; i++) {
-            for (int j = 0; j < str.length - 1; j++) {
-                int a = Integer.parseInt(str[j]);
-                int b = Integer.parseInt(str[j + 1]);
-                if (b > a) {
-                    str[j] = String.valueOf(b);
-                    str[j + 1] = String.valueOf(a);
-                }
-            }
-        }
-        return Integer.parseInt(String.join("", str));
+    return Integer.parseInt(Arrays.stream(String.valueOf(num).split("")).sorted(Comparator.reverseOrder()).collect(Collectors.joining()));
   }
 }
