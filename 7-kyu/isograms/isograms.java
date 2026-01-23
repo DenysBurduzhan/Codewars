4a4d036c1b381fa8feb0b6066154d6892aeeaf65
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-23 15:25:27 +0200

    Update solution - CodeHub

--- 7-kyu/isograms/isograms.java
+++ 7-kyu/isograms/isograms.java
@@ -1,23 +1,5 @@
 public class isogram {
     public static boolean  isIsogram(String str) {
-      if(str.isEmpty()){
-            return true;
-        }
-        String[] s = str.split("");
-        int counter = 0;
-        for(int i = 0; i < s.length; i++){
-            for(int j = 0; j < s.length; j++){
-                String a = String.valueOf(str.charAt(i));
-                String b = String.valueOf(str.charAt(j));
-                if(a.equalsIgnoreCase(b)){
-                    counter++;
-                }
-                if(counter == 2){
-                    return false;
-                }
-            }
-            counter = 0;
-        }
-        return true;
+        return str.toLowerCase().chars().distinct().count() == str.length();
     } 
 }
