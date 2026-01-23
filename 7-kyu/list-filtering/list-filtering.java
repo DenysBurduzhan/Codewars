b99158906120e464312b1c29ab749b9bc4aed7a7
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-23 19:23:30 +0200

    Update solution - CodeHub

--- 7-kyu/list-filtering/list-filtering.java
+++ 7-kyu/list-filtering/list-filtering.java
@@ -1,16 +1,10 @@
 import java.util.List;
-import java.util.ArrayList;
+import java.util.stream.Collectors;
 ​
 public class Kata {
   
- public static List<Object> filterList(final List<Object> list) {
-        List<Object> ints  = new ArrayList<>();
-        for(Object o : list){
-            if(o instanceof  Integer){
-                ints.add(o);
-            }
-        }
-        return ints;
-    }
+  public static List<Object> filterList(final List<Object> list) {
+    return list.stream().filter(i -> i instanceof Integer).collect(Collectors.toList());
+  }
 }
 ​
