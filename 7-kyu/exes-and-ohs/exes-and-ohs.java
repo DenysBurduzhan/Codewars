2519f92fc70a566b1cfe00e4da0cc3798cd3bc9e
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-23 19:49:47 +0200

    Update solution - CodeHub

--- 7-kyu/exes-and-ohs/exes-and-ohs.java
+++ 7-kyu/exes-and-ohs/exes-and-ohs.java
@@ -1,17 +1,8 @@
 public class XO {
   
   public static boolean getXO (String str) {
-        String[] s = str.split("");
-        int counterO = 0;
-        int counterX = 0;
-        for (String i : s){
-            if(i.equalsIgnoreCase("o")){
-                counterO++;
-            }
-            if(i.equalsIgnoreCase("x")){
-                counterX++;
-            }
-        }
-        return counterX == counterO;
-    }
+    
+    return str.toLowerCase().replaceAll("o", "").length() == str.toLowerCase().replaceAll("x", "").length();
+    
+  }
 }
