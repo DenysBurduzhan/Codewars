ead165485a9dcbf33a936f7a5c1e7fb4428d5284
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-24 17:55:45 +0200

    Add solution - CodeHub

---
+++ 7-kyu/get-the-middle-character/get-the-middle-character.java
@@ -0,0 +1,8 @@
+class Kata {
+  public static String getMiddle(String word) {
+        if(word.length() % 2 == 0){
+            return String.valueOf(word.charAt(word.length() / 2 - 1)) + word.charAt(word.length() / 2);
+        }
+        return String.valueOf(word.charAt(word.length() / 2)); // look, no cycles)
+    }
+}
