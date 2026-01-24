7153beda8bb102d17844222bfd09cb4b8ea5948f
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-24 19:03:45 +0200

    Update solution - CodeHub

--- 7-kyu/binary-addition/binary-addition.java
+++ 7-kyu/binary-addition/binary-addition.java
@@ -1,17 +1,6 @@
 public class Kata{
   
   public static String binaryAddition(int a, int b){
-        int sum = a+b;
-        StringBuilder binary =new StringBuilder();
-        while(sum != 0){
-            if(sum % 2 == 0){
-                sum = sum / 2;
-                binary.append("0");
-            }else{
-                sum = sum / 2;
-                binary.append("1");
-            }
-        }
-        return binary.reverse().toString();
-    }
+   return Integer.toBinaryString(a + b);
+  }
 }
