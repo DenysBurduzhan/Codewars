4b6fc8f3b112a81807bb85f5bf4b854eacd0fcf9
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-26 17:31:14 +0200

    Update solution - CodeHub

--- 7-kyu/binary-addition/binary-addition.java
+++ 7-kyu/binary-addition/binary-addition.java
@@ -1,6 +1,9 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 public class Kata{
   
   public static String binaryAddition(int a, int b){
-   return Integer.toBinaryString(a + b);
+    int sum = a + b;
+        return new StringBuilder(IntStream.iterate(sum, n -> n > 0, n -> n / 2).map(n -> n % 2).mapToObj(String :: valueOf).collect(Collectors.joining())).reverse().toString();
   }
 }
