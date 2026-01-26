f03f3f4284ad3b54b6116a1fed73fe506e3d372e
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-26 14:49:51 +0200

    Update solution - CodeHub

--- 7-kyu/mumbling/mumbling.java
+++ 7-kyu/mumbling/mumbling.java
@@ -1,18 +1,7 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 public class Accumul {
     public static String accum(String s) {
-        StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-            if(i > 0){
-                builder.append("-");
-            }
-            for(int j = 0; j <= i; j++){
-                if(j == 0){
-                    builder.append(String.valueOf(s.charAt(i)).toUpperCase());
-                }else {
-                    builder.append(String.valueOf(s.charAt(i)).toLowerCase());
-                }
-            }
-        }
-        return builder.toString();
+       return IntStream.range(0, s.length()).mapToObj(i -> Character.toUpperCase(s.charAt(i)) + String.valueOf(s.charAt(i)).toLowerCase().repeat(i)).collect(Collectors.joining("-"));
     }
 }
