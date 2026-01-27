6c0a7ed73ed4094b1b0631cc46530ced074b10ce
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-01-27 16:05:50 +0200

    Update solution - CodeHub

--- 6-kyu/find-the-odd-int/find-the-odd-int.java
+++ 6-kyu/find-the-odd-int/find-the-odd-int.java
@@ -1,20 +1,6 @@
+import java.util.Arrays;
 public class FindOdd {
     public static int findIt(int[] a) {
-        int counter = 0;
-        for(int i : a){
-            for(int j = 0; j < a.length; j++){
-                if(i == a[j]){
-                    counter++;
-                }
-                if(j == a.length -1){
-                    if(counter % 2 != 0){
-                        return i;
-                    }else{
-                        counter = 0;
-                    }
-                }
-            }
-        }
-        return a[0];
+       return Arrays.stream(a).reduce(0, (i,b) -> i ^ b);
     }
 }
