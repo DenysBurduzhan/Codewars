3f1f5b516531526957a0c25531ef0700fc96bfce
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-02-02 19:23:52 +0200

    Update solution - CodeHub

--- 6-kyu/split-strings/split-strings.java
+++ 6-kyu/split-strings/split-strings.java
@@ -1,18 +1,14 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 public class StringSplit {
     public static String[] solution(String s) {
-        int length = s.length() % 2 == 0 ? s.length() / 2 : s.length() / 2 + 1;
-       String[] str = new String[length];
-        int j = 0;
-       for(int i = 0; i < s.length(); i++){
-           if(i == s.length() - 1){
-               str[j] = s.charAt(i) + "_";
-           }else {
-               str[j] = s.substring(i, i + 2);
-               i += 1;
-               j++;
-           }
+         if(s.length() % 2 == 1){
+           s = s +"_";
        }
-        return str;
+        ArrayList<String> str = new ArrayList<>();
+           for(int i = 0; i < s.length(); i+=2){
+               str.add(s.substring(i, i+2));
+           }
+        return str.toArray(new String[0]);
     }
 }
