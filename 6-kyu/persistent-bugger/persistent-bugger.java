d63e6fa85831ef7b7dc0bbd448e557ebb468bfe7
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-02-04 00:00:27 +0200

    Update solution - CodeHub

--- 6-kyu/persistent-bugger/persistent-bugger.java
+++ 6-kyu/persistent-bugger/persistent-bugger.java
@@ -1,14 +1,16 @@
 class Persist {
-     public static int persistence(long n) {
+  public static int persistence(long n) {
         String str = String.valueOf(n);
         int per = 1;
-        if(str.length() == 1){
-            return 0;
-        }
-        for(int i = 0; i < str.length(); i++){
-            per *= Integer.parseInt(String.valueOf(str.charAt(i)));
-        }
-        return 1 + persistence(per);
+        int counter = 0;
+       while (str.length() != 1) {
+           for (int i = 0; i < str.length(); i++) {
+               per *= Integer.parseInt(String.valueOf(str.charAt(i)));
+           }
+           str = per + "";
+           per = 1;
+           counter++;
+       }
+        return counter;
     }
-  }
-​
+}
