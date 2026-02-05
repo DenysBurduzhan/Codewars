914d296c67a31269a834c1fe47e7eb50c4981178
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-02-05 18:39:02 +0200

    Update solution - CodeHub

--- 5-kyu/rgb-to-hex-conversion/rgb-to-hex-conversion.java
+++ 5-kyu/rgb-to-hex-conversion/rgb-to-hex-conversion.java
@@ -1,40 +1,13 @@
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+​
 public class RgbToHex {
 ​
     public static String rgb(int r, int g, int b) {
-        return convert(r) + convert(g) + convert(b);
+        return Stream.of(r,g, b).map(i -> Math.max(i, 0)).
+                map(i -> Math.min(i, 255)).
+                map(i -> String.format("%02X", i)).collect(Collectors.joining());
     }
-    public static String convert(int i){
-        StringBuilder rgb = new StringBuilder();
-        if(i > 255){
-            return "FF";
-        } else if (i <= 0) {
-            return "00";
-        }
-        String str = "ABCDEF";
-        int i0_parts = i / 16;
-        int i0_remain = i - i0_parts * 16;
-        if(i0_parts >= 10){
-            if(i0_parts == 15){
-                rgb.append("F");
-            }else{
-                int f = i0_parts - 10;
-                rgb.append(str.charAt(f));
-            }
-        }else if (i0_parts < 10){
-            rgb.append(i0_parts);
-        }
-        if(i0_remain >= 10){
-            if(i0_remain == 15){
-                rgb.append("F");
-            }else{
-                int f = i0_remain - 10;
-                rgb.append(str.charAt(f));
-            }
-        }else if (i0_remain < 10){
-            rgb.append(i0_remain);
-        }
 ​
-        return rgb.toString();
-    }
 }
 ​
