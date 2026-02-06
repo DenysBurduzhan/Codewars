23b6547ee028d518b68005ed6c4d6456aee3b983
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-02-06 15:09:02 +0200

    Update solution - CodeHub

--- 5-kyu/human-readable-time/human-readable-time.java
+++ 5-kyu/human-readable-time/human-readable-time.java
@@ -1,29 +1,5 @@
 public class HumanReadableTime {
   public static String makeReadable(int seconds) {
-        if ((seconds == 359999)) {
-            return "99:59:59";
-        }
-        if(seconds < 60 && seconds > 0){
-            String sec = String.valueOf(seconds);
-            if(sec.length() < 2){
-                return "00:"+"00:" + "0" + seconds;
-            }
-            return "00:"+"00:" + seconds;
-        }
-​
-        String hr = String.valueOf(seconds / 3600);
-        if(hr.length() < 2){
-            hr = "0" + hr;
-        }
-        String min = String.valueOf((seconds - Integer.parseInt(hr) * 3600) /60);
-        if(min.length() < 2){
-            min = "0" + min;
-        }
-        String sec = String.valueOf((seconds - Integer.parseInt(hr) * 3600) % 60);
-        if(sec.length() < 2){
-            sec = "0" + sec;
-        }
-​
-        return hr + ":" + min + ":" + sec;
-    }
+    return String.format("%02d:%02d:%02d", seconds / 3600, seconds /60 % 60, seconds  % 60);
+  }
 }
