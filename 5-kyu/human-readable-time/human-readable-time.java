82fbbdeb0029f33db594969ae0cf3e07c5657070
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-02-06 15:17:55 +0200

    Update solution - CodeHub

--- 5-kyu/human-readable-time/human-readable-time.java
+++ 5-kyu/human-readable-time/human-readable-time.java
@@ -1,5 +1,15 @@
+​
+ import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+​
 public class HumanReadableTime {
-  public static String makeReadable(int seconds) {
-    return String.format("%02d:%02d:%02d", seconds / 3600, seconds /60 % 60, seconds  % 60);
+    public static String makeReadable(int seconds) {
+            int hours = seconds / 3600;
+            int minutes = (seconds / 60) % 60;
+            int secs = seconds % 60;
+​
+            return IntStream.of(hours, minutes, secs)
+                    .mapToObj(i -> String.format("%02d", i))
+                    .collect(Collectors.joining(":"));
   }
 }
