5293f3c35ac65b8adad4900482707383eb8526f2
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-02-09 20:36:15 +0200

    Update solution - CodeHub

--- 5-kyu/simple-pig-latin/simple-pig-latin.java
+++ 5-kyu/simple-pig-latin/simple-pig-latin.java
@@ -1,27 +1,10 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+​
 public class PigLatin {
     public static String pigIt(String str) {
-        StringBuilder builder = new StringBuilder();
-        String[] pig = str.split(" ");
-        for(int i = 0; i < pig.length; i++){
-            String[] word = pig[i].split("");
-            for(int f = 0; f < word.length; f++){
-                if (!word[f].matches("[a-zA-Z]")) {
-                    builder.append(word[f]);
-                }else{
-                    if(f+1 == word.length){
-                        if(i == pig.length -1){
-                            builder.append(word[0]).append("ay");
-                        }else{
-                            builder.append(word[0]).append("ay").append(" ");
-                        }
-                    }else{
-                        builder.append(word[f+1]);
-                    }
-                }
-            }
-​
-        }
-​
-        return builder.toString();
+        return Arrays.stream(str.trim().split(" ")).
+                map(s -> s.matches("[a-zA-Z]+")? s.substring(1).concat(s.substring(0,1)).concat("ay") : s).
+                collect(Collectors.joining(" "));
     }
 }
