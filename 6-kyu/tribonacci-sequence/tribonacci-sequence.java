d6b44f1dca3b15f796550840fafe6743af1e4d26
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-02-26 19:13:58 +0200

    Update solution - CodeHub

--- 6-kyu/tribonacci-sequence/tribonacci-sequence.java
+++ 6-kyu/tribonacci-sequence/tribonacci-sequence.java
@@ -3,20 +3,9 @@ import java.util.Arrays;
 public class Xbonacci {
 ​
     public static double[] tribonacci(double[] s, int n) {
-        double[] d = new double[n];
-        if(n <= 3 && n > 0){
-            System.arraycopy(s, 0, d, 0, n);
-            return d;
-        }else if(n < 1){
-            return new double[]{};
-        }
-​
-        for(int i = 0; i < d.length; i++){
-            if(i >= 3){
-                d[i] = d[i -3] + d[i - 2] + d[i -1];
-            }else{
-                d[i] = s[i];
-            }
+        double[] d = Arrays.copyOf(s, n);
+        for(int i = 3; i < d.length; i++){
+            d[i] = d[i-1] + d[i-2] + d[i-3];
         }
         return d;
     }
