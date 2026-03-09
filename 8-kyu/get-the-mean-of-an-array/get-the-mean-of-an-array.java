4af5c6b0beca1bf7f410c5cf8ac15889999da885
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-03-09 23:15:07 +0200

    Add solution - CodeHub

---
+++ 8-kyu/get-the-mean-of-an-array/get-the-mean-of-an-array.java
@@ -0,0 +1,10 @@
+public class School {
+    public static int getAverage(int[] marks) {
+        int sum = 0;
+        for (int mark : marks) {
+            sum += mark;
+        }
+        return sum / marks.length;
+    }
+}
+​
