4738b0985ab4daa80b4f7caa60b7ed3dfdcbdfd3
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-04-05 19:16:32 +0300

    Update solution - CodeHub

--- 6-kyu/find-the-unique-number-1/find-the-unique-number-1.java
+++ 6-kyu/find-the-unique-number-1/find-the-unique-number-1.java
@@ -1,19 +1,9 @@
-import java.util.Arrays;
+​
+ import java.util.Arrays;
 ​
 public class Kata {
     public static double findUniq(double[] arr) {
-        double[] newArr = Arrays.stream(arr).distinct().toArray();
-        for (double v : newArr) {
-            int counter = 0;
-            for (double value : arr) {
-                if (v == value) {
-                    counter++;
-                }
-            }
-            if (counter == 1) {
-                return v;
-            }
-        }
-        return arr[0];
+        Arrays.sort(arr);
+        return arr[0] == arr[1] ? arr[arr.length -1] : arr[0];
     }
 }
