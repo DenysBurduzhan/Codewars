73b580623d9c88cdba6000b8f1fc63711410b093
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-04-07 21:16:09 +0300

    Update solution - CodeHub

--- 6-kyu/backspaces-in-string/backspaces-in-string.java
+++ 6-kyu/backspaces-in-string/backspaces-in-string.java
@@ -1,16 +1,14 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 public class BackspacesInString {
- public static String cleanString(String s) {
-            ArrayList<String> str = new ArrayList<>(Arrays.asList(s.split("")));
-            ArrayList<String> str1 = new ArrayList<>();
-            for(int i = 0; i < str.size(); i++){
-                if(!str.get(i).equals("#")){
-                    str1.add(str.get(i));
-                }else if(!str1.isEmpty()){
-                    str1.remove(str1.size()-1);
-                }
-            }
-            return String.join("" , str1);
+  public static String cleanString(String s) {
+           StringBuilder builder = new StringBuilder();
+           for( int i = 0; i < s.length(); i++ ) {
+               char ch = s.charAt(i);
+               if (ch == '#'){
+                   builder.setLength(Math.max(builder.length() -1, 0));
+               }else{
+                   builder.append(ch);
+               }
+           }
+            return builder.toString();
         }
 }
