7db96bec3ed2318e6a02bc6a92b04edf86b9b422
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-04-16 16:22:46 +0300

    Update solution - CodeHub

--- 7-kyu/sum-of-odd-numbers/sum-of-odd-numbers.java
+++ 7-kyu/sum-of-odd-numbers/sum-of-odd-numbers.java
@@ -1,17 +1,5 @@
 class RowSumOddNumbers {
     public static int rowSumOddNumbers(int n) {
-            int sumOfRow = 0;
-            int sum = 1;
-            int rows = 2;
-            for(int i = 1; i < n; i++){
-                for(int j = 1; j <= rows; j++){
-                    sum += 2;
-                    if(i == n -1){
-                        sumOfRow += sum;
-                    }
-                }
-                rows++;
-            }
-            return n > 1? sumOfRow : n;
-        }
+        return n * n * n;
+    }
 }
