8db26002d55f80c62bdd07cba961433198123a13
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-04-17 18:52:01 +0300

    Update solution - CodeHub

--- 6-kyu/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times.java
+++ 6-kyu/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times.java
@@ -2,22 +2,17 @@ import java.util.*;
 public class EnoughIsEnough {
 ​
   public static int[] deleteNth(int[] elements, int maxOccurrences) {
-        int counter = 0;
-        List<Integer> list = new ArrayList<>();
-        for(int n : elements){
-            list.add(n);
-        }
-        for(int i = 0; i < list.size(); i++) {
-            for(int j = 0; j < list.size(); j++) {
-                if(Objects.equals(list.get(i), list.get(j))){
-                    counter++;
-                    if (counter > maxOccurrences) {
-                        list.remove(j);
-                    }
-                }
+        Map<Integer, Integer> countMap = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+​
+        for (int num : elements) {
+            int count = countMap.getOrDefault(num, 0);
+​
+            if (count < maxOccurrences) {
+                result.add(num);
+                countMap.put(num, count + 1);
             }
-            counter = 0;
         }
-        return maxOccurrences == 0 ? new int[]{} : list.stream().mapToInt(Integer::intValue).toArray();
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
