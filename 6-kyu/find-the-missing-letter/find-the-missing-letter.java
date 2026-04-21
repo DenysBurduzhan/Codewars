6ffc3b25f2c4bafc9ac43866a5116954acc8331c
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-04-21 16:30:00 +0300

    Update solution - CodeHub

--- 6-kyu/find-the-missing-letter/find-the-missing-letter.java
+++ 6-kyu/find-the-missing-letter/find-the-missing-letter.java
@@ -3,12 +3,8 @@ public class Kata
   public static char findMissingLetter(char[] array)
   {
     for(int i = 0; i < array.length; i++){
-           if(i+1 < array.length){
-               int a = array[i];
-               int b = array[i+1];
-               if(b - a > 1){
-                   return (char) (a + 1);
-               }
+           if(array[i + 1] - array[i] > 1){
+               return (char) (array[i] + 1);
            }
        }
     return ' ';
