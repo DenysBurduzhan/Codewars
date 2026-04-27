563e884ceea5ee9b6c3b5b3e93767bd554d9cfdd
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-04-27 19:49:13 +0300

    Update solution - CodeHub

--- 5-kyu/pick-peaks/pick-peaks.java
+++ 5-kyu/pick-peaks/pick-peaks.java
@@ -2,34 +2,27 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
-​
 public class PickPeaks {
+    
+    public static Map<String,List<Integer>> getPeaks(int[] arr) {
+    Map<String,List<Integer>> ans = new HashMap<>() {{
+            put("pos",   new ArrayList<>() );
+            put("peaks", new ArrayList<>() );
+        }};
+        int posMax = 0;
+        boolean matchAsc = false;
 ​
-   public static Map<String,List<Integer>> getPeaks(int[] arr) {
-        List<Integer> picks = new ArrayList<>();
-        List<Integer> pos = new ArrayList<>();
-        Map<String, List<Integer>> map = new HashMap<>();
-        for(int i=0;i<arr.length;i++) {
-            if(i != 0 && i != arr.length-1) {
-                if(arr[i] > arr[i-1] && arr[i] > arr[i+1] ) {
-                    picks.add(arr[i]);
-                    pos.add(i);
-                } else if (arr[i] > arr[i-1] && arr[i] == arr[i+1]) {
-                    int temp = i;
-                    while (i < arr.length -1 && arr[i] == arr[i+1]) {
-                        i++;
-                    }
-                    if(i != arr.length-1 ) {
-                        if (arr[i] > arr[i + 1]) {
-                            picks.add(arr[i]);
-                            pos.add(temp);
-                        }
-                    }
-                }
+        for (int i = 1 ; i < arr.length ; i++) {
+            if (arr[i-1] < arr[i]) {
+                matchAsc = true;
+                posMax = i;
+            }
+            if (matchAsc && arr[i-1] > arr[i]) {
+                matchAsc = false;
+                ans.get("pos").add(posMax);
+                ans.get("peaks").add(arr[posMax]);
             }
         }
-        map.put("pos",pos);
-        map.put("peaks",picks);
-        return map;
+        return ans;
     }
 }
