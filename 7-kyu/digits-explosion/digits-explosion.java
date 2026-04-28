010022f18d19df39ec90e726e90c5625cc9590eb
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-04-28 19:04:58 +0300

    Update solution - CodeHub

--- 7-kyu/digits-explosion/digits-explosion.java
+++ 7-kyu/digits-explosion/digits-explosion.java
@@ -1,19 +1,10 @@
-import java.util.HashMap;
-​
 public class Solution {
-    public static String explode(String digits) {
-        HashMap<String, Integer> map = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
+  public static String explode(String digits) {
+     StringBuilder sb = new StringBuilder();
         for (int i = 0; i < digits.length(); i++) {
             int value = Integer.parseInt(digits.charAt(i) + "");
-            String key = String.valueOf(value);
-            if(!map.containsKey(key)) {
-                map.put(key, value);
-            }else{
-                map.put(key, map.get(key) * value);
-            }
             sb.append(String.valueOf(digits.charAt(i)).repeat(Math.max(0, value)));
         }
         return sb.toString();
-    }
+  }
 }
