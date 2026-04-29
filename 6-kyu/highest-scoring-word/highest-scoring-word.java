dcf99525af29fa8fbc98f0231de1c3a90ad5ef54
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-04-29 18:24:30 +0300

    Update solution - CodeHub

--- 6-kyu/highest-scoring-word/highest-scoring-word.java
+++ 6-kyu/highest-scoring-word/highest-scoring-word.java
@@ -1,31 +1,11 @@
 import java.util.*;
+​
 public class Kata {
 ​
-  public static String high(String s) {
-        int sum = 0;
-       String[] words = s.split(" ");
-        int[] num = new int[words.length];
-       for (int j = 0; j <  words.length; j++ ) {
-           String word = words[j];
-           for(int i = 0; i < word.length(); i++) {
-               sum += word.charAt(i) + 1 - 'a';
-           }
-           num[j] = sum;
-           sum = 0;
-       }
-       for(int i = 0; i <= num.length ; i++ ) {
-           for( int j = 0; j < num.length; j++ ) {
-               if (j + 1 < num.length && num[j] < num[j + 1]) {
-                   int temp = num[j];
-                   num[j] = num[j + 1];
-                   num[j + 1] = temp;
-                   String tempS = words[j];
-                   words[j] = words[j + 1];
-                   words[j + 1] = tempS;
-               }
-           }
-       }
-        return words[0];
+    public static String high(String s) {
+        return Arrays.stream(s.split(" ")).
+                max(Comparator.comparingInt(
+                        word -> word.chars().map(c -> c - 'a' + 1).sum())).
+                orElse("");
     }
-​
 }
