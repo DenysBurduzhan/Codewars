bff4e9a0419f1f2ddf05740d36ce9fa29e6ce239
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-05-01 21:14:08 +0300

    Update solution - CodeHub

--- 7-kyu/flatten-and-sort-an-array/flatten-and-sort-an-array.java
+++ 7-kyu/flatten-and-sort-an-array/flatten-and-sort-an-array.java
@@ -1,18 +1,9 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-​
 public class Kata {
 ​
-    public static int[] flattenAndSort(int[][] array) {
-        List<Integer> result = new ArrayList<>();
-        for (int[] ints : array) {
-            for (int anInt : ints) {
-                result.add(anInt);
-            }
-        }
-        Collections.sort(result);
-        return result.stream().mapToInt(Integer::intValue).toArray();
-    }
+  public static int[] flattenAndSort(int[][] array) {   
+     return Arrays.stream(array).flatMapToInt(Arrays::stream).sorted().toArray();
+  }
+​
 }
+​
