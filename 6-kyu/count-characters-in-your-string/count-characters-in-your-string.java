fb491dcdd8535ebb9dc21cc2962b6a9a354a17b7
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-05-01 18:01:59 +0300

    Update solution - CodeHub

--- 6-kyu/count-characters-in-your-string/count-characters-in-your-string.java
+++ 6-kyu/count-characters-in-your-string/count-characters-in-your-string.java
@@ -1,16 +1,10 @@
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 ​
 public class Kata {
     public static Map<Character, Integer> count(String str) {
-        Map<Character, Integer> map = new HashMap<>();
-        for(char c : str.toCharArray()) {
-            if(!map.containsKey(c)) {
-                map.put(c, 1);
-            }else {
-                map.put(c, map.get(c) + 1);
-            }
-        }
-        return map;
+        return str.chars()
+               .mapToObj(c -> (char) c)
+               .collect(Collectors.toMap(c -> c, c -> 1, Integer::sum));
     }
 }
