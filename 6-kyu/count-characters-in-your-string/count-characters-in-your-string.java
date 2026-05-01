28ce878b4cb21063cff1c26c86e549c706c19fe7
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-05-01 18:08:46 +0300

    Update solution - CodeHub

--- 6-kyu/count-characters-in-your-string/count-characters-in-your-string.java
+++ 6-kyu/count-characters-in-your-string/count-characters-in-your-string.java
@@ -3,8 +3,9 @@ import java.util.stream.Collectors;
 ​
 public class Kata {
     public static Map<Character, Integer> count(String str) {
-        return str.chars()
+       return str.chars()
                .mapToObj(c -> (char) c)
-               .collect(Collectors.toMap(c -> c, c -> 1, Integer::sum));
+               .collect(Collectors.groupingBy(c -> c , Collectors.summingInt(c -> 1)));
     }
 }
+​
