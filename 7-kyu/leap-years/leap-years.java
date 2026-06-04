ba83dd383f4d841fc7ba2bd4f2b91c89c9b097af
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-06-04 21:04:26 +0300

    Update solution - CodeHub

--- 7-kyu/leap-years/leap-years.java
+++ 7-kyu/leap-years/leap-years.java
@@ -1,6 +1,7 @@
-public class LeapYears {
+import java.time.Year;
 ​
+public class LeapYears {
   public static boolean isLeapYear(int year) {
-    return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    return Year.of(year).isLeap();
   }
 }
