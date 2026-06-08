97843d393e9f7dbc5e1d3bfc52e101a019619e1c
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-06-08 17:55:56 +0300

    Update the-paperfold-sequence.java

--- 6-kyu/the-paperfold-sequence/the-paperfold-sequence.java
+++ 6-kyu/the-paperfold-sequence/the-paperfold-sequence.java
@@ -1,19 +1,10 @@
 import java.util.function.IntSupplier;
 ​
 public class PaperFold implements IntSupplier {
-   private int index = 0;
-    private StringBuilder seq = new StringBuilder("1");
-​
     @Override
     public int getAsInt() {
-        while (index >= seq.length()) {
-            StringBuilder next = new StringBuilder(seq);
-            next.append('1');
-            for (int i = seq.length() - 1; i >= 0; i--) {
-                next.append(seq.charAt(i) == '1' ? '0' : '1');
-            }
-            seq = next;
-        }
-        return seq.charAt(index++) - '0';
+        i++;
+        return (i & Integer.lowestOneBit(i) << 1) == 0 ? 1 : 0;
     }
+   int i = 0;
 }
