199fcdceb0e36beb701f81e25c819fc0671a1dda
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-06-08 17:46:36 +0300

    Update solution - CodeHub

--- 7-kyu/caffeine-script/caffeine-script.java
+++ 7-kyu/caffeine-script/caffeine-script.java
@@ -1,10 +1,5 @@
 public class CaffeineBuzz {
     public static String caffeineBuzz(int n) {
-        if (n % 3 == 0) {
-            String result = (n % 4 == 0) ? "Coffee" : "Java";
-            if (n % 2 == 0) result += "Script";
-            return result;
-        }
-        return "mocha_missing!";
+        return n % 12 == 0 ? "CoffeeScript" : n % 6 == 0 ? "JavaScript" : n % 3 == 0 ? "Java" : "mocha_missing!";
     }
 }
