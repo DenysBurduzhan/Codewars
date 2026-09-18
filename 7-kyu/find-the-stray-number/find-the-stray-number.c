3c1856ff3fe244b11ce550cc90749ec5db21c265
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-09-18 20:33:39 +0300

    Update solution - CodeHub

--- 7-kyu/find-the-stray-number/find-the-stray-number.c
+++ 7-kyu/find-the-stray-number/find-the-stray-number.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
 #include <stddef.h>
+#include <stdio.h>
 ​
 int stray(size_t n, const int arr[n]) {
-    int result = 0;
-    for (size_t i = 0; i < n; i++) {
-        result ^= arr[i];
-    }
-    return result;
+int res = 0;
+for(int i = 0; i < n; i++){
+    res ^= arr[i];
+}
+    return res;
 }
