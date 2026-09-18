c1b5baf4e44159f2013ce109689f7ff1047f1e90
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-09-18 20:49:31 +0300

    Update solution - CodeHub

--- 7-kyu/breaking-chocolate-problem/breaking-chocolate-problem.c
+++ 7-kyu/breaking-chocolate-problem/breaking-chocolate-problem.c
@@ -1,8 +1,5 @@
 #include <stdint.h>
 ​
 uint32_t breaking_chocolate(uint32_t n, uint32_t m) {
-    if((n < 1 || m < 1) || (n == 1 && m == 1)){
-        return 0;
-    }
-    return n * m -1;
+    return n && m ? n * m - 1 : 0;
 }
